4c12bde07ccbcfa8b1acf40757bf963b6e8d0940
igorbocor9 <igor.bocor9@gmail.com>
2026-08-21 18:41:52 -0300

    fixing player texture and level randomizer

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -14,7 +14,7 @@ public class LevelManager : MonoBehaviour
     private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
     private LevelPieceBasedSetup _currSetup;
 
-    private void Awake()
+    private void Start()
     {
         //SpawnNextLevel();
         CreateLevelPieces();
