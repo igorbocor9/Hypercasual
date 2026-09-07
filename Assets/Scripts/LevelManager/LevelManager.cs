b994842e94416d0dfaa76561d5b1f02459e21ed6
igorbocor9 <igor.bocor9@gmail.com>
2026-09-07 14:54:17 -0300

    adding wave to piece spawn

--- Assets/Scripts/LevelManager/LevelManager.cs
+++ Assets/Scripts/LevelManager/LevelManager.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
+using DG.Tweening;
 
 public class LevelManager : MonoBehaviour
 {
@@ -11,8 +12,14 @@ public class LevelManager : MonoBehaviour
     public float timeBetweenPieces = .3f;
     [SerializeField] private int _index;
     private GameObject _currentLevel;
-    private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
+
+    [SerializeField]private List<LevelPieceBase> _spawnedPieces = new List<LevelPieceBase>();
     private LevelPieceBasedSetup _currSetup;
+
+    [Header("Animation")]
+    public float scaleDuration = .2f;
+    public float scaleTimeBetweenPieces = .1f;
+    public Ease ease = Ease.OutBack;
 
     private void Start()
     {
@@ -85,6 +92,24 @@ public class LevelManager : MonoBehaviour
         }
 
         ColorManager.Instance.ChangeColorByType(_currSetup.artType);
+
+        StartCoroutine(ScalePiecesByTime());
+    }
+
+    private IEnumerator ScalePiecesByTime()
+    {
+        foreach(var p in _spawnedPieces)
+        {
+            p.transform.localScale = Vector3.zero;
+        }
+
+        yield return null;
+
+        for (int i = 0; i < _spawnedPieces.Count; i++)
+        {
+            _spawnedPieces[i].transform.DOScale(1, scaleDuration).SetEase(ease);
+            yield return new WaitForSeconds(scaleTimeBetweenPieces);
+        }
     }
 
     private void CreateLevelPiece(List<LevelPieceBase> list)
